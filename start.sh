5ebbb911c74c1c508d0d03a41ded4442146ba718
yejunyu <yejunyu@wps.cn>
2019-02-28 15:47:47 +0800

    start

--- start.sh
+++ start.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si;
-
 echo "\n[archlinuxcn]\n\nSigLevel = Optional TrustedOnly\nServer=https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/$arch" |sudo tee -a /etc/pacman.conf;
 
-sudo pacman -Syy && sudo pacman -S archlinuxcn-keyring;
+sudo pacman -Syy && sudo pacman -S archlinuxcn-keyring yay;
 
-yay -Sy fcitx-sogoupinyin fcitx-im fcitx-configtool deepin.com.qq.office electronic-wechat deepin.com.thunderspeed google-chrome nutstore shadowsocks-qt5 wps-office ttf-wps-fonts;
+yay -Sy vim fcitx-sogoupinyin fcitx-im fcitx-configtool deepin.com.qq.office electronic-wechat deepin.com.thunderspeed google-chrome nutstore shadowsocks-qt5 wps-office ttf-wps-fonts picgo-appimage typora autojump;
 
 echo "export GTK_IM_MODULE=fcitx\nexport QT_IM_MODULE=fcitx\nexport XMODIFIERS=\"@im=fcitx\"" >> ~/.xprofile;
 
 cp -f ./user-dirs.dirs ~/.config/user-dirs.dirs;
+
+yay -S zsh && sudo chsh -s $(which zsh)
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+
+cp -f ./robbyrussell.zsh-theme ~/.oh-my-zsh/themes/
+
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+cp -f ./zconfig ~/.zshrc
 
